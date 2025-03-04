c707e428f011bcfb99ed0c66640358f0c06f84c6
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-03-04 13:58:23 +0100

    feat: Async i2c operation

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@
 
 use embassy_executor::Spawner;
 use embassy_nrf::{
-    bind_interrupts, peripherals::{self, SERIAL0}, twim::{self, Twim}
+    bind_interrupts, gpio::AnyPin, peripherals::{self, SERIAL0}, twim::{self, Twim}
 };
 
 use embassy_nrf::gpio::{Level, Output, OutputDrive, Pin};
 use embassy_time::Timer;
 
-use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
+use embassy_embedded_hal::shared_bus::asynch::i2c::{self, I2cDevice};
 use embassy_sync::mutex::Mutex;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use static_cell::StaticCell;
@@ -74,8 +74,9 @@ async fn my_i2c_task(mut twi: Twim<'static, peripherals::SERIAL0>) {
 }*/
 
 #[embassy_executor::task]
-async fn npm1300_task(i2c_dev1::I2cDevice<'static, SERIAL0>) {
-    let mut npm1300 = NPM1300::new(i2c_dev1, embassy_time::Delay);
+async fn npm1300_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>) {
+    
+    let mut npm1300 = NPM1300::new(i2c_dev, embassy_time::Delay);
     defmt::info!("Configuring LED modes...");
     let _ = npm1300.configure_led0_mode(LedMode::Host).await;
     defmt::info!("Configured LED0 mode to Host");
@@ -101,19 +102,86 @@ async fn npm1300_task(i2c_dev1::I2cDevice<'static, SERIAL0>) {
     let _ = npm1300.enable_battery_recharge().await;
     let _ = npm1300.configure_ibat_measurement(true).await;
 
-    let vbat_voltage = npm1300.measure_vbat().await.unwrap();
-    let _ = npm1300.measure_ntc().await;
-    let ntc_temp = npm1300.get_ntc_measurement_result().await.unwrap();
-    let die_temp = npm1300.measure_die_temperature().await.unwrap();
-    let ibat_current = npm1300.measure_ibat().await.unwrap();
-    defmt::info!("NTC Temp: {:?}", ntc_temp);
-    defmt::info!("Die Temp: {:?}", die_temp);
-    defmt::info!("VBAT Voltage: {:?}", vbat_voltage);
-    defmt::info!("IBAT Current: {:?}", ibat_current);
+    loop {
+        let vbat_voltage = npm1300.measure_vbat().await.unwrap();
+        let _ = npm1300.measure_ntc().await;
+        let ntc_temp = npm1300.get_ntc_measurement_result().await.unwrap();
+        let die_temp = npm1300.measure_die_temperature().await.unwrap();
+        let ibat_current = npm1300.measure_ibat().await.unwrap();
+        defmt::info!("NTC Temp: {:?}", ntc_temp);
+        defmt::info!("Die Temp: {:?}", die_temp);
+        defmt::info!("VBAT Voltage: {:?}", vbat_voltage);
+        defmt::info!("IBAT Current: {:?}", ibat_current);
+        Timer::after_millis(10000).await;
+    };
+}
+
+#[embassy_executor::task]
+async fn gas_sensor_task(
+        heater_pin: AnyPin, 
+        sensor_pin: AnyPin,
+        led_pin1:AnyPin,
+        led_pin2:AnyPin,
+        i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>
+    ) {
+
+    let mut led1 = Output::new(led_pin1, Level::Low, OutputDrive::Standard);
+    let mut led2 = Output::new(led_pin2, Level::Low, OutputDrive::Standard);
+    let mut power = Output::new(heater_pin, Level::Low, OutputDrive::Standard);
+    let mut sensor = Output::new(sensor_pin, Level::Low, OutputDrive::Standard);
+
+    let adsconfig = ADS111xConfig::default()
+        .mux(InputMultiplexer::AIN0GND)
+        .dr(DataRate::SPS860)
+        .pga(ProgramableGainAmplifier::V2_048);
+
+    let mut adc = match ADS111x::new(i2c_dev, 0x48u8, adsconfig){
+        Err(e) => panic!("Error {:?}", e),
+        Ok(x) => x,
+    };
+
+    let _ = adc.write_config(None).await;
+
+    loop {
+        led1.set_high();
+        power.set_high();
+        Timer::after_millis(98).await;
+
+        led2.set_high();
+        sensor.set_high();
+        let volt =  adc.read_single_voltage(None).await.unwrap();
+
+        sensor.set_low();
+        power.set_low();
+        led1.set_low();
+        led2.set_low();
+
+        defmt::info!("Voltage: {:?}", volt);
+        Timer::after_millis(900).await;
+    }
+
+}
+
+#[embassy_executor::task]   
+async fn bme680_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>)
+ {
+
+    let bme680config =  Configuration::builder()
+        .temperature_oversampling(Oversampling::By2)
+        .pressure_oversampling(Oversampling::By16)
+        .humidity_oversampling(Oversampling::By1)
+        .filter(IIRFilter::Coeff1)
+        .gas_config(None)
+        .build();
+
+    
+    let mut bme680 = AsyncBme680::new(i2c_dev, DeviceAddress::Primary, embassy_time::Delay,25);
+
+    let _ = bme680.set_configuration(&bme680config).await;
 }
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     defmt::info!("Starting NPM1300 example");
     let p = embassy_nrf::init(Default::default());
     
@@ -127,11 +195,6 @@ async fn main(_spawner: Spawner) {
     let scl_pin = p.P0_29.degrade();
     let twi_port = p.SERIAL0;
 
-    let mut led1 = Output::new(led_pin1, Level::Low, OutputDrive::Standard);
-    let mut led2 = Output::new(led_pin2, Level::Low, OutputDrive::Standard);
-    let mut power = Output::new(heater_pin, Level::Low, OutputDrive::Standard);
-    let mut sensor = Output::new(sensor_pin, Level::Low, OutputDrive::Standard);
-
     let mut config = twim::Config::default();
     // Modify the i2c configuration fields if you dont have external i2c pullups
     config.sda_pullup = true;
@@ -141,55 +204,19 @@ async fn main(_spawner: Spawner) {
     
     let i2c = Twim::new(twi_port, Irqs, sda_pin, scl_pin, config);
     let i2c_bus = Mutex::new(i2c);
+
     let i2c_bus = I2C_BUS.init(i2c_bus);
 
     let i2c_dev1 = I2cDevice::new(i2c_bus);
-    
+    spawner.spawn(npm1300_task(i2c_dev1)).unwrap();
 
     let i2c_dev2 = I2cDevice::new(i2c_bus);
-
-    let adsconfig = ADS111xConfig::default()
-        .mux(InputMultiplexer::AIN0GND)
-        .dr(DataRate::SPS860)
-        .pga(ProgramableGainAmplifier::V2_048);
-
-    let mut adc = match ADS111x::new(i2c_dev2, 0x48u8, adsconfig){
-        Err(e) => panic!("Error {:?}", e),
-        Ok(x) => x,
-    };
-
-    let _ = adc.write_config(None).await;
-
-    let bme680config =  Configuration::builder()
-                         .temperature_oversampling(Oversampling::By2)
-                         .pressure_oversampling(Oversampling::By16)
-                         .humidity_oversampling(Oversampling::By1)
-                         .filter(IIRFilter::Coeff1)
-                         .gas_config(None)
-                         .build();
+    spawner.spawn(gas_sensor_task(heater_pin,sensor_pin,led_pin1,led_pin2,i2c_dev2)).unwrap();
     
     let i2c_dev3 = I2cDevice::new(i2c_bus);
-    let mut bme680 = AsyncBme680::new(i2c_dev3, DeviceAddress::Primary, embassy_time::Delay,25);
-
-    let _ = bme680.set_configuration(&bme680config).await;
-
+    spawner.spawn(bme680_task(i2c_dev3)).unwrap();
     
 
-    loop {
-        led1.set_high();
-        
-        power.set_high();
-        Timer::after_millis(98).await;
-        led2.set_high();
-        sensor.set_high();
-        let volt =  adc.read_single_voltage(None).await.unwrap();
-        sensor.set_low();
-        power.set_low();
-        led1.set_low();
-        led2.set_low();
 
-        defmt::info!("Voltage: {:?}", volt);
-        Timer::after_millis(900).await;
-    }
 
 }
