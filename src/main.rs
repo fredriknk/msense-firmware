eff87cb620733e6a08aeee4f9e0ecfdcfde8343a
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-03-05 13:38:07 +0100

    feat: Try out modem

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@
 
 //! Example demonstrating control of the NPM1300 PMIC's leds
 
+use core::{array, error, ops::Index, time, u64};
+
 use embassy_executor::Spawner;
 use embassy_nrf::{
-    bind_interrupts, 
-    gpio::AnyPin, 
-    peripherals::{self, SERIAL0}, 
-    twim::{self, Twim}
+    bind_interrupts, gpio::{AnyPin, Input}, pac::rtc::regs::Counter, peripherals::{self, SERIAL0}, twim::{self, Twim}
 };
 
 use embassy_nrf::gpio::{
@@ -29,9 +28,14 @@ use embassy_sync::{
         NoopRawMutex,
         CriticalSectionRawMutex,
     },
-    channel::Channel,
+    channel::{
+        Channel,
+        Sender,
+        Receiver
+    },
     signal::Signal, 
     mutex::Mutex,
+    blocking_mutex::raw::ThreadModeRawMutex,
 };
 use static_cell::StaticCell;
 
@@ -69,27 +73,63 @@ bind_interrupts!(struct Irqs {
     SERIAL0 => twim::InterruptHandler<peripherals::SERIAL0>;
 });
 
-struct BmeData {
+struct EnvData {
     /// Temperature in °C
     temperature: f32,
     /// Relative humidity in %
     humidity: f32,
     /// Pressure in hPa
     pressure: f32,
+    /// counter
+    bmecounter: u32,
+    /// Gas resistance in Ohms
+    gas_resistance: f32,
+    /// Gas resistance counter
+    gascounter: u32,
+
+    
+}
+struct SingleSampleStorage {
+    temperature: f32,
+    humidity: f32,
+    pressure: f32,
+    gas_resistance: f32,
+    timestamp: u64,
 }
 
+struct BatteryStatus{
+    battery_voltage: f32,
+    battery_current: f32,
+    battery_temperature: f32,
+    timestamp: u64,
+}
+
+enum BatteryTrigger {
+    TriggerBatteryRead,
+    StartCharging,
+}
+
+const DATASTORE_SIZE: usize = 2;
+const TEMP_INTERVAL: u64 = 2;
+const BATTERYLOOPTIME: u64 = 10000;
+const NUM_SAMPLES_PER_AGGREGATION: u64 = 12;
+const NUM_SAMPLES_PER_BATTERY_READ: u64 = 24;
+
+static GAS_CHANNEL: StaticCell<Channel<NoopRawMutex, SingleSampleStorage, DATASTORE_SIZE>> = StaticCell::new();
+static BATTERY_STATUS_CHANNEL: StaticCell<Channel<NoopRawMutex, BatteryStatus, DATASTORE_SIZE>> = StaticCell::new();
 
 static GAS_SIGNAL: Signal<CriticalSectionRawMutex, bool> = Signal::new();
 
-static BME_SIGNAL: Signal<CriticalSectionRawMutex, bool> = Signal::new();
+static BATTERY_SIGNAL: Signal<CriticalSectionRawMutex, BatteryTrigger> = Signal::new();
 
-static GAS_RESULT_SIGNAL: Signal<CriticalSectionRawMutex, f32> = Signal::new();
-
-static BME_RESULT_SIGNAL: Signal<CriticalSectionRawMutex, BmeData> = Signal::new();
+static GAS_RESULT_SIGNAL: Signal<CriticalSectionRawMutex, EnvData> = Signal::new();
 
 
 #[embassy_executor::task]
-async fn npm1300_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>) {
+async fn npm1300_task(
+        i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>,
+        sender: Sender<'static, NoopRawMutex, BatteryStatus, DATASTORE_SIZE>,
+    ) {
     
     let mut npm1300 = NPM1300::new(i2c_dev, embassy_time::Delay);
     defmt::info!("Configuring LED modes...");
@@ -109,7 +149,7 @@ async fn npm1300_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SE
 
     defmt::info!("Configuring Charging...");
     let _ = npm1300.set_charger_current(300).await;
-    let _ = npm1300.set_termination_current_level(ChargerTerminationCurrentLevelSelect::SEL10).await;
+    let _ = npm1300.set_termination_current_level(ChargerTerminationCurrentLevelSelect::SEL20).await;
     let _ = npm1300.set_normal_temperature_termination_voltage(ChargerTerminationVoltage::V4_20).await;
     let _ = npm1300.set_warm_temperature_termination_voltage(ChargerTerminationVoltage::V4_10).await;
     let _ = npm1300.set_discharge_current_limit(DischargeCurrentLimit::High).await;
@@ -118,18 +158,40 @@ async fn npm1300_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SE
     let _ = npm1300.configure_ibat_measurement(true).await;
 
     loop {
-        let _ = npm1300.enable_led0().await;
-        let vbat_voltage = npm1300.measure_vbat().await.unwrap();
-        let _ = npm1300.measure_ntc().await;
-        let ntc_temp = npm1300.get_ntc_measurement_result().await.unwrap();
-        let die_temp = npm1300.measure_die_temperature().await.unwrap();
-        let ibat_current = npm1300.measure_ibat().await.unwrap();
-        defmt::info!("NTC Temp: {:?}", ntc_temp);
-        defmt::info!("Die Temp: {:?}", die_temp);
-        defmt::info!("VBAT Voltage: {:?}", vbat_voltage);
-        defmt::info!("IBAT Current: {:?}", ibat_current);
-        let _ = npm1300.disable_led0().await;
-        Timer::after_millis(10000).await;
+        let battsig = BATTERY_SIGNAL.wait().await;
+        match battsig {
+            BatteryTrigger::TriggerBatteryRead => {
+                defmt::info!("Triggering Battery Measurement");
+                let vbat_voltage = npm1300.measure_vbat().await.unwrap();
+                let _ = npm1300.measure_ntc().await;
+                let ntc_temp = npm1300.get_ntc_measurement_result().await.unwrap();
+                let die_temp = npm1300.measure_die_temperature().await.unwrap();
+                let ibat_current = npm1300.measure_ibat().await.unwrap();
+                let status = npm1300.get_charger_status().await.unwrap();
+                defmt::info!("Charger Status: {:?}", status);
+                defmt::info!("NTC Temp: {:?}, Die Temp: {:?}, VBAT Voltage: {:?}, IBAT Current: {:?}", ntc_temp,die_temp,vbat_voltage,ibat_current);
+
+                let battery_data = BatteryStatus {
+                    battery_voltage: vbat_voltage,
+                    battery_current: ibat_current,
+                    battery_temperature: ntc_temp,
+                    timestamp: Instant::now().as_millis(),
+                };
+
+                let err = sender.try_send(battery_data);
+                if err.is_err() {
+                    defmt::warn!("Battery Queue full");
+                }
+                else {
+                    defmt::info!("Battery Data sent");
+                }
+            },
+            BatteryTrigger::StartCharging => {
+                defmt::info!("Charging Battery");
+                let _ = npm1300.set_vbus_in_current_limit(VbusInCurrentLimit::MA1000).await;
+                BATTERY_SIGNAL.signal(BatteryTrigger::TriggerBatteryRead);
+            }
+        }
     };
 }
 
@@ -139,53 +201,16 @@ async fn gas_sensor_task(
         sensor_pin: AnyPin,
         led_pin1:AnyPin,
         led_pin2:AnyPin,
-        i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>
+        i2c_dev1: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>,
+        i2c_dev2: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>,
+        sender: Sender<'static, NoopRawMutex, SingleSampleStorage, DATASTORE_SIZE>,
     ) {
-
+    
     let mut led1 = Output::new(led_pin1, Level::Low, OutputDrive::Standard);
     let mut led2 = Output::new(led_pin2, Level::Low, OutputDrive::Standard);
     let mut power = Output::new(heater_pin, Level::Low, OutputDrive::Standard);
     let mut sensor = Output::new(sensor_pin, Level::Low, OutputDrive::Standard);
-
-    let adsconfig = ADS111xConfig::default()
-        .mux(InputMultiplexer::AIN0GND)
-        .dr(DataRate::SPS860)
-        .pga(ProgramableGainAmplifier::V2_048);
-
-    let mut adc = match ADS111x::new(i2c_dev, 0x48u8, adsconfig){
-        Err(e) => panic!("Error {:?}", e),
-        Ok(x) => x,
-    };
-
-    let _ = adc.write_config(None).await;
-
-    loop {
-        let _ = GAS_SIGNAL.wait().await;
-        led1.set_high();
-        power.set_high();
-        let start = Instant::now();
-        Timer::after_millis(98).await;
-        defmt::info!("Gas Sensor Measurement took: {:?}", start.elapsed().ticks() * 1_000_000 / TICK_HZ;);
-        led2.set_high();
-        sensor.set_high();
-        
-        let volt =  adc.read_single_voltage(None).await.unwrap();
-        
-        sensor.set_low();
-        power.set_low();
-        led1.set_low();
-        led2.set_low();
-
-        defmt::debug!("Voltage: {:?}", volt);
-        GAS_RESULT_SIGNAL.signal(volt);
-    }
-
-}
-
-#[embassy_executor::task]   
-async fn bme680_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SERIAL0>>)
- {
-
+    
     let bme680config =  Configuration::builder()
         .temperature_oversampling(Oversampling::By2)
         .pressure_oversampling(Oversampling::By16)
@@ -194,28 +219,94 @@ async fn bme680_task(i2c_dev: I2cDevice<'static, NoopRawMutex, Twim<'static, SER
         .gas_config(None)
         .build();
 
-    
-    let mut bme680 = AsyncBme680::new(i2c_dev, DeviceAddress::Primary, embassy_time::Delay,25);
+
+    let mut bme680 = AsyncBme680::new(i2c_dev2, DeviceAddress::Primary, embassy_time::Delay,25);
 
     let _ = bme680.initialize(&bme680config).await;
 
+    let adsconfig = ADS111xConfig::default()
+        .mux(InputMultiplexer::AIN0GND)
+        .dr(DataRate::SPS860)
+        .pga(ProgramableGainAmplifier::V2_048);
+
+    let mut adc = match ADS111x::new(i2c_dev1, 0x48u8, adsconfig){
+        Err(e) => panic!("Error {:?}", e),
+        Ok(x) => x,
+    };
+
+    let _ = adc.write_config(None).await;
+
+    let mut counter: u64 = 0;
+
+    let mut sensor_data    = EnvData {
+        temperature: 0.0,
+        humidity: 0.0,
+        pressure: 0.0,
+        bmecounter: 0,
+        gas_resistance: 0.0,
+        gascounter: 0,
+    };
     loop {
-        let _ = BME_SIGNAL.wait().await;
-        Timer::after_millis(3).await; // Wait for the gas sensor to have started
-        let start = Instant::now();
-        let data = bme680.measure().await.unwrap();
-        let _ = bme680.put_to_sleep().await;
-        defmt::info!("Measurement took: {:?}", start.elapsed());
-        defmt::info!("Temperature: {:?}", data.temperature);
-        defmt::info!("Pressure: {:?}", data.pressure);
-        defmt::info!("Humidity: {:?}", data.humidity);
-        defmt::info!("Gas Resistance: {:?}", data.gas_resistance);
-        BME_RESULT_SIGNAL.signal(BmeData{
-            temperature: data.temperature,
-            humidity: data.humidity,
-            pressure: data.pressure,
-        });
+        let _ = GAS_SIGNAL.wait().await;
+        led1.set_high();
+        power.set_high();
         
+        Timer::after_millis(98).await;
+        
+        led2.set_high();
+        sensor.set_high();
+        
+        let volt =  adc.read_single_voltage(None).await.unwrap();
+
+        sensor.set_low();
+        power.set_low();
+        led1.set_low();
+        led2.set_low();
+
+        sensor_data.gas_resistance = volt;
+        sensor_data.gascounter += 1;
+
+        if counter%TEMP_INTERVAL == 0 {
+            let data = bme680.measure().await.unwrap();
+            sensor_data.temperature += data.temperature;
+            sensor_data.pressure += data.pressure;
+            sensor_data.humidity += data.humidity;
+            sensor_data.bmecounter += 1;
+            defmt::debug!("Temperature: {:?}", data.temperature);
+            defmt::debug!("Pressure: {:?}", data.pressure);
+            defmt::debug!("Humidity: {:?}", data.humidity);
+        }
+        defmt::debug!("Voltage: {:?}", volt);
+        if counter%NUM_SAMPLES_PER_AGGREGATION == 0 {
+            defmt::info!("Sending data...");
+            let data_send   = SingleSampleStorage {
+                temperature: sensor_data.temperature / sensor_data.bmecounter as f32,
+                humidity: sensor_data.humidity / sensor_data.bmecounter as f32,
+                pressure: sensor_data.pressure / sensor_data.bmecounter as f32,
+                gas_resistance: sensor_data.gas_resistance / sensor_data.gascounter as f32,
+                timestamp: Instant::now().as_millis(),
+            };
+
+            let err = sender.try_send(data_send);
+            if err.is_err() {
+                defmt::warn!("Sample Queue full");
+            }
+            else {
+                defmt::info!("Sample Data sent");
+            }
+            sensor_data = EnvData {
+                temperature: 0.0,
+                humidity: 0.0,
+                pressure: 0.0,
+                bmecounter: 0,
+                gas_resistance: 0.0,
+                gascounter: 0,
+            };
+        }
+        if counter%NUM_SAMPLES_PER_BATTERY_READ == 0 {
+            BATTERY_SIGNAL.signal(BatteryTrigger::TriggerBatteryRead);
+        }
+        counter += 1;
     }
 }
 
@@ -224,7 +315,16 @@ async fn tictoctrigger() {
     loop{
         Timer::after_millis(2000).await;
         GAS_SIGNAL.signal(true);
-        BME_SIGNAL.signal(true);
+    }
+}
+
+#[embassy_executor::task]
+async fn charge_interrupt(host_pin: AnyPin) {
+    let mut host = Input::new(host_pin, embassy_nrf::gpio::Pull::Down);
+    loop {
+        host.wait_for_rising_edge().await;
+        BATTERY_SIGNAL.signal(BatteryTrigger::StartCharging);
+        Timer::after_millis(1000).await;
     }
 }
 
@@ -239,31 +339,33 @@ async fn main(spawner: Spawner) {
     let heater_pin = p.P0_27.degrade();
     let sensor_pin = p.P0_31.degrade();
 
+    let host_pin = p.P0_04.degrade();
+
     let sda_pin = p.P0_28.degrade();
     let scl_pin = p.P0_29.degrade();
     let twi_port = p.SERIAL0;
 
+    let gas_channel = GAS_CHANNEL.init(Channel::new());
+    let battery_status_channel = BATTERY_STATUS_CHANNEL.init(Channel::new());
+
+    defmt::info!("Configuring TWIM...");
     let mut config = twim::Config::default();
     // Modify the i2c configuration fields if you dont have external i2c pullups
     config.sda_pullup = true;
     config.scl_pullup = true;
-
-    defmt::info!("Configuring TWIM...");
-    
     let i2c = Twim::new(twi_port, Irqs, sda_pin, scl_pin, config);
     let i2c_bus = Mutex::new(i2c);
     let i2c_bus = I2C_BUS.init(i2c_bus);
-
     let i2c_dev1 = I2cDevice::new(i2c_bus);
-    spawner.spawn(npm1300_task(i2c_dev1)).unwrap();
+
+    spawner.spawn(npm1300_task(i2c_dev1,battery_status_channel.sender())).unwrap();
+
+    spawner.spawn(charge_interrupt(host_pin)).unwrap();
 
     let i2c_dev2 = I2cDevice::new(i2c_bus);
-    spawner.spawn(gas_sensor_task(heater_pin,sensor_pin,led_pin1,led_pin2,i2c_dev2)).unwrap();
-    
     let i2c_dev3 = I2cDevice::new(i2c_bus);
-    spawner.spawn(bme680_task(i2c_dev3)).unwrap();
-
+    spawner.spawn(gas_sensor_task(heater_pin,sensor_pin,led_pin1,led_pin2,i2c_dev2,i2c_dev3,gas_channel.sender())).unwrap();
+    
     spawner.spawn(tictoctrigger()).unwrap();
 
-    
 }
