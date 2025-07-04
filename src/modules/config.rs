dc4a1c2c02a851eaea9dc8fac7b0b414366a693c
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-07-04 13:02:24 +0200

    Fix: Changed datastore to 44 and temp reading every 2 samples

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -2,9 +2,9 @@ pub const HOST_ADDRESS : &str = "iot.nekko.no"; //Server address
 pub const TCP_PORT: u16 = 8443; //Server port
 pub const CERT_CHAIN: u32 = 16842753; // Certificate chain ID in NRF
 
-pub const DATASTORE_SIZE: usize = 30; // How many samples can be stored in the channel
+pub const DATASTORE_SIZE: usize = 44; // How many samples can be stored in the channel
 pub const ERR_CAP: usize = 10; // How many error messages can be stored in the error log channel
-pub const TEMP_INTERVAL: u64 = 1; // How many gas samples are taken between every temp reading
+pub const TEMP_INTERVAL: u64 = 2; // How many gas samples are taken between every temp reading
 pub const MS_PER_SAMPLE: u64 = 30000; // How many milliseconds between every sample
 pub const MS_FILAMENT_HEAT: u64 = 98; // How many milliseconds to heat the filament before taking a sample
 pub const MS_SENSOR_HEAT: u64 = 3; // How many milliseconds to heat the sensor before taking a sample
