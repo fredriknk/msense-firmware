1aa53ae24ee51d4c92a527da7314ad480e809c94
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-07-04 12:50:19 +0200

    Fix: Cleanup

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -2,11 +2,11 @@ pub const HOST_ADDRESS : &str = "iot.nekko.no"; //Server address
 pub const TCP_PORT: u16 = 8443; //Server port
 pub const CERT_CHAIN: u32 = 16842753; // Certificate chain ID in NRF
 
-pub const DATASTORE_SIZE: usize = 64; // How many samples can be stored in the channel
+pub const DATASTORE_SIZE: usize = 32; // How many samples can be stored in the channel
 pub const ERR_CAP: usize = 5; // How many error messages can be stored in the error log channel
 pub const TEMP_INTERVAL: u64 = 1; // How many gas samples are taken between every temp reading
 pub const MS_PER_SAMPLE: u64 = 30000; // How many milliseconds between every sample
-pub const NUM_SAMPLES_PER_AGGREGATION: u64 = 2; // How many samples are aggregated before sending to the channel
+pub const NUM_SAMPLES_PER_AGGREGATION: u64 = 10; // How many samples are aggregated before sending to the channel
 pub const NUM_SAMPLES_PER_BATTERY_READ: u64 = 20; // How many samples are taken before reading the battery
 
 pub const NUM_MINUTES_PER_SEND: u64 = 30; // How many minutes between sending data to the server
