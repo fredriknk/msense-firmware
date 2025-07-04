64c06c4261cd3c38564eb378f4075cfd76bbb172
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-07-04 14:19:20 +0200

    Feat: Remove unused features

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -4,9 +4,13 @@ pub const CERT_CHAIN: u32 = 16842753; // Certificate chain ID in NRF
 
 pub const DATASTORE_SIZE: usize = 44; // How many samples can be stored in the channel
 pub const ERR_CAP: usize = 10; // How many error messages can be stored in the error log channel
+#[cfg(any(feature = "rev_1_3_2", feature = "rev_2_0"))]
 pub const TEMP_INTERVAL: u64 = 2; // How many gas samples are taken between every temp reading
 pub const MS_PER_SAMPLE: u64 = 30000; // How many milliseconds between every sample
+
+#[cfg(any(feature = "rev_1_3_2", feature = "rev_2_0"))]
 pub const MS_FILAMENT_HEAT: u64 = 98; // How many milliseconds to heat the filament before taking a sample
+#[cfg(any(feature = "rev_1_3_2", feature = "rev_2_0"))]
 pub const MS_SENSOR_HEAT: u64 = 3; // How many milliseconds to heat the sensor before taking a sample
 pub const NUM_SAMPLES_PER_AGGREGATION: u64 = 10; // How many samples are aggregated before sending to the channel
 pub const NUM_SAMPLES_PER_BATTERY_READ: u64 = 30; // How many samples are taken before reading the battery
